ec095670f651de901fa1fa9656c1dd3a83180a88
Sandra84-code <57835837+Sandra84-code@users.noreply.github.com>
2019-12-07 19:24:02 +0100

    Update views.py

--- sanitt/views.py
+++ sanitt/views.py
@@ -5,32 +5,83 @@
 
 # Create your views here.
 import pubchempy
-from django.http import HttpResponse
+import pygtop
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Target, Protein, Gene, Compound, Compoundcid
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from pubchempy import get_compounds
-from pubchempy import Compound
+from .pubchempy import get_compounds
+from .pubchempy import Compound
 from django.views.decorators.csrf import csrf_protect
-from .forms import CompoundForm 
+from .forms import CompoundForm, TargetForm
+#from .iuphartarget import Target
+#from django.db.models import Q
+
 
 @csrf_protect
-def index(request):
-    ''' This could be your actual view or a new one '''
-   
-    if request.method == 'GET': 
 
-        search_query = CompoundForm(request.GET)
+def searchcompound(request):
+
+    if request.method == 'GET':
+
+        csearch = CompoundForm(request.GET)
+
+        def compoundsearch(csearch):
+            for compound in get_compounds('{csearch}','name'):
+                ccid=compound.cid
+                if ccid.is_valid():
+                    
+                     return render(request,'sanitt/index.html',{'csearch':csearch, 'ccid':ccid})
+
+                else:
+
+                     return HttpResponse('/doesntwork/')
+
+    return render(request,'sanitt/index.html')
+
+@csrf_protect
+
+def searchtarget(request):
+    
+    if request.method == 'POST':
+
+        tsearch = TargetForm(request.POST)
+
+        def targetsearch(tsearch):
+
+            mytarget={"name":tsearch}
+            tname = pygtop.get_targets_by(mytarget)
+
+            if mytarget.is_valid():
+                target_id=tname.id
+                contextt={
+                    'tsearch':tsearch,
+                    'tname':tname,
+                    'target_id':tname.id
+                    }
+
+                return render(request,'sanitt/index.html',)
+
+            else:
+
+                return HttpResponse('/doesntwork/')
+               
         
-        if search_query.is_valid():
-            return HttpResponseRedirect('/thanks/')
-        else:
-            return HttpResponseRedirect('/doesntwork/')
+    return render(request,'sanitt/detail.html')
+
+def indextarget(request):
+    all_targets = pygtop.get_all_targets()
+    len_targets = len(all_targets)
     latest_target_list=Target.objects.order_by('-tname')[:5]
-    context={'latest_target_list': latest_target_list}
-    return render(request,'sanitt/index.html',context)
+    context={
+        'latest_target_list': latest_target_list, 
+        'all_targets': all_targets,
+        'len_targets': len_targets
+         } 
+    return render(request,'sanitt/alltargets.html',context)
+
 
 def detail(request, target_id):
     uniprot=get_object_or_404(Target, pk=target_id)
@@ -45,15 +96,6 @@ def detail(request, target_id):
         }
     return render(request, 'sanitt/detail.html',contextd )
 
-#def indexc(request):
-    
-    
-
-   
-
-    #latest_compound_list=Compound.objects.order_by('-compoundname')[:5]
-    #contextc={'latest_compound_list':latest_compound_list}
-    #return render(request,'sanitt/index.html',contextc)
 
 def resultsc(request, compoundname):
     def compoundsearch(csearch):
