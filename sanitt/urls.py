8485f18a9e47ea0b61f40ec3cdad280aba7513da
Sandra84-code <57835837+Sandra84-code@users.noreply.github.com>
2019-12-20 00:51:02 +0100

    Update urls.py

--- sanitt/urls.py
+++ sanitt/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 app_name='sanitt'
 urlpatterns=[
-    url(r'^$',views.searchcompound, name='index'),
-    url(r'^$',views.searchtarget, name='index'),
+    url(r'^$',views.index, name='index'),
+    url(r'^compound/$',views.searchcompound, name='searchcomp'),
+    url(r'^target/$',views.searchtarget, name='detail'),
     url(r'^alltargets/$',views.indextarget, name='alltargets'),
     url(r'^compound/(?P<compoundname>[0-9]+)/$',views.resultsc, name='resultsc'),
-    url(r'^compound/(?P<ccid>[0-9]+)/$',views.detailc, name='detailc'),
     url(r'^target/(?P<target_id>[0-9]+)/$',views.detail, name='detail'),
 ]
